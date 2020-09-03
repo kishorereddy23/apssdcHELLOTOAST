b0b87007183a60a4926ac381b2b95b526ea7cd41
kishorereddy23 <70091012+kishorereddy23@users.noreply.github.com>
2020-09-03 13:09:51 +0530

    Initial commit

---
+++ app/src/main/java/com/kishore/hellotoast/MainActivity.java
@@ -0,0 +1,50 @@
+package com.kishore.hellotoast;
+
+import androidx.annotation.MainThread;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mTextView;
+    Button t_button,c_button;
+    int c=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView=findViewById(R.id.tv);
+        t_button=findViewById(R.id.toast_button);
+        c_button=findViewById(R.id.count_button);
+        if (savedInstanceState!=null && savedInstanceState.containsKey("key name")){
+            c=savedInstanceState.getInt("key name");
+            mTextView.setText(String.valueOf(c));
+        }
+        t_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"welcome to app development",Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+        c_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            c++;
+            mTextView.setText(String.valueOf(c));
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("key name",c);
+    }
+}
